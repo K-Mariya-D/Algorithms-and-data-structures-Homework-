925e139487aee2d174b089fbda1954d2aa3744ab
Мария Корчагина <kmd2oo619@gmail.com>
2025-03-01 16:37:46 +0300

    Реализованы методы erase, Equals, lower/upper_bound, next/prev

--- Work3/Work3/BinaryTree.h
+++ Work3/Work3/BinaryTree.h
@@ -16,6 +16,41 @@ class BinaryTree {
 private:
 	Node<T>* root = nullptr;
 	size_t size = 0;
+
+	//Удаление узла. Вспомогательный для erase
+	Iterator deleteElen(Node<value_type>* cur) {
+		if (cur->left == nullptr && cur->right == nullptr)
+		{
+			if (cur->parent->left == cur)
+				cur->parent->left = nullptr;
+			else cur->parent->right = nullptr;
+			return nullptr;
+		}
+		else if (cur->left != nullptr && cur->right != nullptr)
+		{
+			Node<value_type>* next = cur->right;
+			while (next->left != nullptr)
+				next = next->left;
+			std::swap(next->data, cur->data);
+			delete(next);
+		}
+		else
+		{
+			auto pos = cur->parent->left == cur ? cur->parent->left : cur->parent->right;
+			auto next = cur->left != nullptr ? cur->left : cur->right;
+			pos = next;
+			return pos;
+		}
+	}
+	//Сравнение 2х деревьев начиная с поданных узлов на равенство. (Лучше сделать для итераторов)
+	// Вспомогательный для Equals 
+	bool equals(Node<value_type>* first, Node<value_type>* second) {
+		if (first == nullptr && second == nullptr)
+			return true;
+		if (first->data != second->data)
+			return false;
+		else return equals(first->left, second->left) && equals(first->left, second->left);
+	}
 public:
 	using value_type = T;
 	using size_type = size_t;
@@ -112,6 +147,14 @@ public:
 	Iterator end() {
 		return Iterator(nullptr);
 	}
+	//Итератор на следующий элемент (по значению data)
+	Iterator next(Iterator cur) {
+		return ++cur;
+	}
+	//Итератор на предыдущий элемент (по значению data)
+	Iterator prev(Iterator cur) {
+		return --cur;
+	}
 	void Add(const value_type& val) {
 		size++;
 
@@ -168,11 +211,45 @@ public:
 			++it;
 		return it;
 	}
-	/*
+
 	Iterator erase(Iterator pos) {
 		if (root == nullptr)
 			throw std::out_of_range("BinaryTree size should be more than 0!");
 		Node<value_type>* cur = root;
-
-	}*/
+		while (cur->data != *pos || cur != nullptr)
+		{
+			if (*pos > cur->data)
+				cur = cur->right;
+			else if (*pos < cur->data)
+				cur = cur->left;
+		}
+		if (cur == nullptr)
+			throw std::invalid_argument("Tree hasn't such iteraror!");
+		cur = delete(cur);
+		return Iterator(cur);
+	}
+	//Итератор на первый элемент больше val
+	Iterator upper_bound(const value_type& val) {
+		Iterator it = begin();
+		while (*it <= val && it != end())
+			++it;
+		return it;
+	}
+	//Итератор на первый элемент меньше val
+	Iterator lower_bound(const value_type& val) {
+		Iterator it = begin();
+		while (*it >= val && it != end())
+			--it;
+		return it;
+	}
+	/*
+	//Печатает в поряке Левый/Текущий/Правый
+	void printLCR(Iterator p) {
+		
+	}
+	*/
+	friend bool Equals(const BinaryTree<T>& tree1, const BinaryTree<T>& tree2) {
+		return equals(tree1.root, tree2.root);
+	}
+	
 };
